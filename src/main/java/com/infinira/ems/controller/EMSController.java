f5afad8102c8f9cecb3043166920de77add72a3a
Manmathan MS <manmathan.selvam-ext@jci.com>
2023-03-20 12:07:17 +0530

    Revert "CORS Policy added"
    This reverts commit 618c3c8e7c9bcd0af9ff731c674ff9e313ec0c8b.

--- src/main/java/com/infinira/ems/controller/EMSController.java
+++ src/main/java/com/infinira/ems/controller/EMSController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.MediaType;
 import java.util.Optional;
 import java.util.List;
@@ -19,7 +18,6 @@ import com.infinira.ems.service.EMSService;
 
 @RestController
 @RequestMapping(path="/ems")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EMSController {
 	@Autowired
 	private EMSService emsService;
